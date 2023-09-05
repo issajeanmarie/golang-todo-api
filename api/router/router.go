cd74ce67addc897534ca8fcc793253a08a19572b
Issa Jean Marie <jeanmarieissa@gmail.com>
2023-09-05 11:23:11 +0200

    delete endpoint

--- api/router/router.go
+++ api/router/router.go
@@ -6,10 +6,14 @@ import (
 	consts "todo-api/config"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetupRouter() *gin.Engine{
 	r := gin.Default()
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	
 
 
 	port := os.Getenv("PORT"); 
